2cd67b78e9506c64db56af405e6159ce9d34ac9d
feiguang.cao <feiguang.cao@quanr.com>
2020-04-18 12:25:57 +0800

    init

---
+++ src/main/java/com/ef00/Application.java
@@ -0,0 +1,12 @@
+package com.ef00;
+
+/**
+ * @author feiguang.cao
+ * @date 2020-04-18 12:23
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
